a88f3d34c24ab937fcdc7d5d71d0511f2621d7b2
Chalkybot <chalkbot@pm.me>
2024-02-21 16:27:55 +0200

    feat: Fully working version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::path::PathBuf;
 use sha2::{Sha256, Digest};
 use serde::{Deserialize, Serialize};
-
+use itertools::Itertools;
+use clap::Parser;
 
 #[derive(Debug,Serialize, Deserialize)]
 struct RemarkableMetadata {
@@ -19,7 +20,6 @@ struct RemarkableMetadata {
     pinned: bool,
 }
 
-
 #[derive(Debug)]
 struct FileWithHash { 
     path: PathBuf,
@@ -34,12 +34,19 @@ struct RemarkableObj {
     file_paths: Vec<FileWithHash>,
 }
 
+#[derive(Parser)]
+struct Cli { 
+    #[clap(default_value = "/home/user/Documents/remarkable-filesync-rust/remarkable-library/")]
+    path_to_library: PathBuf,
+}
+
+
 impl FileWithHash { 
     fn from(path: PathBuf) -> Self {
         let hash: Option<String> = match FileWithHash::generate_file_hash(&path) {
             Ok(t) => Some(t),
             // Manage other error kinds. Right now this is just silent.
-            Err(ref e) => None,
+            Err(_) => None,
     
         };
         FileWithHash {
@@ -57,7 +64,6 @@ impl FileWithHash {
 }
 
 impl RemarkableObj {
-    // This is better for my use than using the PatBuf.set_extension as that requires a temporary mutable.
     fn set_file_extension(original_path: &PathBuf, extension: &str) -> PathBuf {
         let mut temp_path = original_path.clone();
         temp_path.set_extension(extension);
@@ -71,7 +77,9 @@ impl RemarkableObj {
         let mut entire_hash = Sha256::new();
         
         for path in file_paths {
+            match path.extension() { Some(t) => (), None => continue, }
             if path.extension().unwrap().to_str().unwrap() == "metadata" { 
+                println!("{:?}",&path);
                 uuid = String::from(RemarkableObj::set_file_extension(&path, "")
                                                                         .file_name()
                                                                         .unwrap()
@@ -89,7 +97,7 @@ impl RemarkableObj {
         };
         let hash_text = format!("{:X}", entire_hash.finalize());
         let metadata_class: RemarkableMetadata = match std::fs::read_to_string(&metadata_path) {
-            Ok(Data) => serde_json::from_str(Data.as_str()).unwrap(),
+            Ok(data) => serde_json::from_str(data.as_str()).unwrap(),
             Err(e) => panic!("{e}"),
         }; 
         RemarkableObj { 
@@ -100,48 +108,36 @@ impl RemarkableObj {
             file_paths: files,
         }
     }
-
-}
-fn list_files(vec: &mut Vec<PathBuf>, path: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
-    if std::fs::metadata(&path)?.is_dir() {
-        let paths = std::fs::read_dir(&path)?;
-        for path_result in paths {
-            let full_path = path_result?.path();
-            if std::fs::metadata(&full_path)?.is_dir() {
-                list_files(vec, &full_path)?
-            } else {
-                vec.push(full_path);
-            }
-        }
-    }
-    Ok(())
 }
 
-
-fn fetch_library(path: &str)  { 
-    let path = PathBuf::from(path);
-    let mut files = Vec::new();
-    list_files(&mut files, &path);
+fn group_directory_contents(path: &PathBuf) -> Result<Vec<(String, Vec<PathBuf>)>, std::io::Error> {
+    // Let's fetch the contents of the folder and return it as a Vec of Vecs.
+    let entire_content = std::fs::read_dir(path)?;
+    let paths: Vec<PathBuf> = entire_content.into_iter().map(|x| x.unwrap().path()).collect();
+    let groups: Vec<(String, Vec<PathBuf>)> = paths
+        .into_iter()
+        .group_by(|path| path
+                        .file_stem()
+                        .unwrap_or_default()
+                        .to_str()
+                        .unwrap_or_default()
+                        .to_owned())
+        .into_iter()
+        .map(|(prefix, group)| (prefix, group.collect()))
+        .collect();
+    Ok(groups)
 }
 
 
 fn main() {
-/*    let paths = vec![
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d/fc818769-7c76-4db2-8f48-15b04db4b8e9.rm"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d/63f9a996-b6b6-4f92-b11d-54dc30058e3c.rm"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d/3b7cf0cd-6d7f-427d-a9af-65d01f5098bc.rm"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d.content"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d.epub"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d.epubindex"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d.metadata"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d.pagedata"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d.pdf"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d.thumbnails"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d.thumbnails/3b7cf0cd-6d7f-427d-a9af-65d01f5098bc.png"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d.thumbnails/fc818769-7c76-4db2-8f48-15b04db4b8e9.png"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d.thumbnails/4484edc4-1629-4692-ac28-05d038b70730.png"),
-                PathBuf::from("remarkable-library/f1f5c131-ec35-4e79-8e23-2092412aa75d.thumbnails/63f9a996-b6b6-4f92-b11d-54dc30058e3c.png")]; */
-    let path = "/home/user/Documents/remarkable-filesync-rust/remarkable-library";
-     fetch_library(path);
+    let args = Cli::parse();
+    
+    //let path = "/home/user/Documents/remarkable-filesync-rust/remarkable-library/";
+    let x = group_directory_contents(&args.path_to_library);
+    dbg!(&x);
+    for (key, list_of_files) in x.unwrap() { 
+        let y = RemarkableObj::new(list_of_files);
+        dbg!(y);
 
+    }
 }
